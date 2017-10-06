bf53d98535827f3a463e4ab84efcb5ba9256936e
pachav76 <pacha.v76@yandex.ru>
2017-10-06 19:04:24 +0300

    classwork

--- learn.js
+++ learn.js
@@ -1,3 +1,19 @@
-var apples = '2';
-var oranges = '3';
-alert ( apples + oranges );
+var Name = prompt('Who that?','');
+
+if (Name == 'Admin') {
+    var pass = prompt ('Password','');
+
+    if (pass == 'BlackMaster') {
+        alert( 'Welcome!' );
+    } else if (pass == null) {
+        alert ('Acces denied');
+    }
+    else {
+        alert ('Password wrong');
+    }
+
+} else if (Name == null) {
+    alert ('Acces denied');
+} else {
+    alert ('I dont know you');
+}
