a2e39c8438c955bce13feb1b502d317f42ba4087
pachav76 <pacha.v76@yandex.ru>
2017-10-07 22:46:20 +0300

    classwork

--- homework3/firstmaket.js
+++ homework3/firstmaket.js
@@ -1,9 +1,13 @@
+var page = 0;
+var itemsCount = 9;
+
 $(document).ready(function () {
     $.ajax({
         url: 'https://jsonplaceholder.typicode.com/photos',
         method: 'GET',
         success: function (data, status, xhr) {
-            renderProducts(data.slice(0, 9));
+            renderProducts(data.slice(page, itemsCount));
+            page++;
         },
         error: function () {
 
@@ -11,9 +15,25 @@ $(document).ready(function () {
     });
 });
 
+$('.show-more-buttom .fa-plus-circle').click(showMoreClicked);
+
+function showMoreClicked() {
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/photos',
+        method: 'GET',
+        success: function (data, status, xhr) {
+            renderProducts(data.slice(page * itemsCount, (page + 1) * itemsCount));
+            page++;
+        },
+        error: function () {
+
+        }
+    }); 
+}
+
 function renderProducts(data) {
     
-    var parentElem = $('.product-gallery')[0];
+    var parentElem = $('.product-gallery-container')[0];
     var elem;
     for (i = 0; i < data.length; i++) {
         elem = document.createElement('div');
@@ -22,3 +42,16 @@ function renderProducts(data) {
         parentElem.append(elem);
     }
 }
+
+function fillCarousel(data) {
+var parentElem = $('.carousel-inner');
+var elem;
+for (i = 0; i < data.length; i++) {
+    parentElem.append (
+    '<div class="carousel-item' + ((i === 0) ? ' active' : '') + '">' +
+        `<img class="d-block w-100" src="` + data[i].url + `" alt="` + data[i].title + `">
+      </div>`
+    );
+    }
+}
+
